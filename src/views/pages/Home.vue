837cd1b40579914627dc8bb7981b0963dd72c874
zhangqiuchi <geochi@qq.com>
2021-06-12 17:20:28 +0800

    :sparkles: update Shopcart.vue

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <swiper :imgList="imageList"></swiper>
+    <div class="floor-menu">
+      <div v-for="(item, index) in menus" :key="index" class="floor-menu-item">
+        <img :src="item.image" alt="">
+        <p>{{ item.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,23 +22,67 @@ export default {
       imageList: [
         {
           id: 1,
-          src: require("@/assets/swipeImg1.jpg"),
+          src: require("@/assets/swipeImg1.jpg")
         },
         {
           id: 2,
-          src: require("@/assets/swipeImg2.jpg"),
+          src: require("@/assets/swipeImg2.jpg")
         },
         {
           id: 3,
-          src: require("@/assets/swipeImg3.png"),
-        },
+          src: require("@/assets/swipeImg3.png")
+        }
       ],
+      menus: [
+        {
+          name: "超市",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        },
+        {
+          name: "数码电器",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        },
+        {
+          name: "充值缴费",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        },
+        {
+          name: "京东商城",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        },
+        {
+          name: "领券",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        },
+        {
+          name: "会员中心",
+          image: require("@/assets/home/e2bcc411170524f0.png.webp")
+        }
+      ]
     };
-  },
+  }
 };
 </script>
 <style lang="scss" scoped>
 .home {
   background-color: aquamarine;
+
+  .floor-menu {
+    display: flex;
+    flex-wrap: wrap;
+
+    .floor-menu-item {
+      margin: 2px;
+      display: block;
+      width: 75px;
+      height: 75px;
+      text-align: center;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
